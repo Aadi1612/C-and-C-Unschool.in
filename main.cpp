800cf4293cb48f6b8a4495312bf5f46fb1e63d81
Aadi1612 <85367292+Aadi1612@users.noreply.github.com>
2021-09-03 23:34:08 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+float area(float x)
+{
+    float areac;
+    cout<<"Circle"<<endl;
+    cout <<"Enter radius: ";
+    cin >> x;
+    areac = 3.14 * x * x;
+    cout << "Area of circle: "<< areac;
+}
+
+float area(float x, float y)
+{
+    float areat;
+    cout<<"\nTriangle";
+    cout<<"\nEnter length: ";
+    cin>>x;
+    cout<<"Enter height: ";
+    cin >> y;
+    areat = 0.5 * x * y;
+    cout << "area of triangle: "<< areat;
+}
+
+int area(int x, int y)
+{
+    int arear;
+    cout<<"\nRectangle"<<"\nEnter length: ";
+    cin>>x;
+    cout<<"Enter breadth: ";
+    cin>>y;
+    arear = x * y;
+    cout<<"Area of Rectangle: "<< arear;
+}
+
+int main()
+{
+    int d,e;
+    float a, b, c;
+    area(a);
+    cout<<"\n";
+    area(b,c);
+    cout<<"\n";
+    area(d,e);
+    return 0;
+}
